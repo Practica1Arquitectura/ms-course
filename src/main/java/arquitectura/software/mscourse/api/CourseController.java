5e21315593df54c248e4eb0863eb30444d472b7d
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2021-11-15 12:08:17 -0400

    se agrego la configuracion del LOGGER mas zipkin y sleuth

--- src/main/java/arquitectura/software/mscourse/api/CourseController.java
+++ src/main/java/arquitectura/software/mscourse/api/CourseController.java
@@ -5,6 +5,8 @@ import arquitectura.software.mscourse.entity.Student;
 import arquitectura.software.mscourse.repository.CourseRepository;
 import arquitectura.software.mscourse.service.CourseService;
 import arquitectura.software.mscourse.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/v3/api/course")
 public class CourseController {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(CourseController.class);
 
     @Autowired
     private CourseRepository courseRepository;
@@ -30,6 +34,7 @@ public class CourseController {
 
     @RequestMapping(path = "/save",method = RequestMethod.POST)
     public Course saveCourse(@RequestBody Course course){
+        LOGGER.info("Registrando un curso: con los siguientes datos, {} ",course);
         return courseRepository.save(course);
     }
 
